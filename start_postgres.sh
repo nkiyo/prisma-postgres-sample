50532cc8b1b315ba8dd3877246733dcde1742e39
nkiyo <nakahara.kiyoshi@gmail.com>
2026-05-01 18:52:46 +0900

    up

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,5 +1,9 @@
-docker run --name postgres \
-    -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password \
-    -p 5432:5432 \
-    -v postgres-volume:/var/lib/postgresql/data \
-    -d postgres:17.2
+if [ "$(docker container inspect -f '{{.Name}}' postgres 2>/dev/null)" = "/postgres" ]; then
+    docker start postgres
+else
+    docker run --name postgres \
+        -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password \
+        -p 5432:5432 \
+        -v postgres-volume:/var/lib/postgresql/data \
+        -d postgres:17.2
+fi
